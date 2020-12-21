0ee11ed0dbcfea1e7e39c7c21d3c28e0fd3b207d
Ziad Zebib <zebibziad@gmail.com>
2020-12-21 22:07:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  arrays! = array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
