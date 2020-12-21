85921676424e0f57eb5c31b225ec5a31c0c51341
Ziad Zebib <zebibziad@gmail.com>
2020-12-21 21:47:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array<< "e"
+  add_element_to_end_of_array.push("e") 
 end
 
 def add_element_to_start_of_array(array, element)
