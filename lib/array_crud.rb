7d1a80891ddda3d8204b4c4387676f74e738532e
Ziad Zebib <zebibziad@gmail.com>
2020-12-21 21:59:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_end_of_array = ["array", "element"]
-  add_element_to_end_of_array.unshift("arrays!")
+  add_element_to_start_of_array_of_array = ["array", "element"]
+  add_element_to_start_of_array.unshift("arrays!")
 end
 
 def remove_element_from_end_of_array(array)
