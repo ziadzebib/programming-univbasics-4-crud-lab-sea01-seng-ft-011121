22b037aefce2dd1887dac6813f9992e9c85fbcd6
Ziad Zebib <zebibziad@gmail.com>
2020-12-21 22:11:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,7 +37,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  arrays[5]
 end
 
 def update_element_from_index(array, index_number, element)
